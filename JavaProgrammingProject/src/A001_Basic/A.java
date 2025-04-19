46eaa492e1bfccd5c7818b086879c03c595fe0c9
Akhileshrazz <akhileshrazz310@gmail.com>
2025-04-19 12:17:55 +0530

    lelo

---
+++ JavaProgrammingProject/src/A001_Basic/A.java
@@ -0,0 +1,8 @@
+package A001_Basic;
+
+public class A {
+public static void main(String[] args) {
+	System.out.println("A");
+	System.out.println("B");
+}
+}
--- JavaProgrammingProject/src/String/A001.java
+++ JavaProgrammingProject/src/String/A001.java
@@ -1,7 +1,18 @@
 package String;
 
 public class A001 {
-public static void main(String[] args) {
-	System.out.println("hello");
-}
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("Hii");
+		System.out.println("Hii");
+		System.out.println("Hii");
+		System.out.println("Hii");
+		System.out.println("Hii");
+		System.out.println("Hii");
+		System.out.println("Hii");
+		System.out.println("Hii");
+		System.out.println("Hii");
+		System.out.println("loo");
+		System.out.println("Hello");
+	}
 }
